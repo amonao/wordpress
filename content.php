7474a4762f3379e240594036bf696019b0f1f6ea
Amina O <85079430+amonao@users.noreply.github.com>
2021-09-18 16:28:21 +0200

    Add files via upload

---
+++ content.php
@@ -0,0 +1,11 @@
+   <div class="blog-post">
+            <h2 class="blog-post-title"><?php the_title(); ?></h2>
+            <p class="blog-post-meta"><?php the_date(); ?><a href="#"><?php the_author(); ?></a></p>
+            <?php the_content(); ?>
+
+            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <hr>
+            
+          </div><!-- /.blog-post -->
+
+          
